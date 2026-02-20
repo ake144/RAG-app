d1de364d506542525f060b3c092c2ce30fd2965b
ake144 <tamiratgedi1@gmail.com>
2026-02-20 22:04:23 +0300

    feat: add message component and fetchSources function with basic layout

---
+++ components/chat/message.tsx
@@ -0,0 +1,6 @@
+const message={
+    "role": "user",
+    "content": "What is the meaning of life?"
+}
+
+export default message;
---
+++ components/chat/sources.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default async function fetchSources() {
+    return(
+<View>
+    <Text>Sources</Text>
+</View>
+    )
+}
